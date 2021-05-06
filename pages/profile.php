7c284144e4a0efd2d0df91238d3136b8deedcc46
unknown <melv.douc@gmail.com>
2021-05-06 03:53:14 +0200

    content set dynamically using HTMLContent class

--- pages/profile.php
+++ pages/profile.php
@@ -1,21 +1,36 @@
 <?php
-  if(!checkSession("connected")):
-    exit("You're not logged in.");
-  endif;
-  
-  require "./db.php";
-  $id = $_SESSION["userID"];
-  if(checkSession("is_admin") && isset($_GET["id"]))
-    $id = (INT)$_GET["id"];
-  
-  $user = $db->query("SELECT * FROM users WHERE id = $id;");
-  $user = $user->fetch();
+if (!checkSession("connected")) :
+  exit("You're not logged in.");
+endif;
 
-  if(!$user)
-    exit("No user with this ID.");
+require "./db.php";
+$id = $_SESSION["userID"];
+if (checkSession("is_admin") && isset($_GET["id"]))
+  $id = (int)$_GET["id"];
 
-  extract($user);
-  $imgSrc = (!$avatar) ? "default.jpg" : $avatar;
+$user = $db->query("SELECT * FROM users WHERE id = $id;");
+$user = $user->fetch();
+
+if (!$user)
+  exit("No user with this ID.");
+
+extract($user);
+$imgSrc = (!$avatar) ? "default.jpg" : $avatar;
+
+$idForAdmin = new HTMLContent(checkSession("is_admin"), "&id=$id");
+
+$userOnlyLinks = new HTMLContent(
+  $id === $_SESSION["userID"],
+  "<a href='./?update-password'><button>Update Password</button></a>
+    <a href='./processing.php?sign-out'><button>Log Out</button></a>",
+);
+
+$deleteAccountButton = new HTMLContent(
+  checkSession("is_admin"),
+  "<a href='./processing.php?delete-account&id=$id'>
+    <button class='btn-red'>Delete Account</button>
+    </a>"
+);
 ?>
 
 <h2><?= $username ?>'s Profile</h2>
@@ -36,26 +51,16 @@
       <td><?= $username ?></td>
       <td><?= $email ?></td>
       <td>
-        <img
-          height="50"
-          src="./assets/img/<?= $imgSrc ?>"
-          alt="<?= $username ?>'s Profile Picture"
-        >
+        <img height="50" src="./assets/img/<?= $imgSrc ?>" alt="<?= $username ?>'s Profile Picture">
       </td>
       <td><?= $register_date ?></td>
     </tr>
   </tbody>
 </table>
 
-<a href="./?update-profile<?php if(checkSession("is_admin")) echo "&id=$id"; ?>"><button>Update Profile</button></a>
+<a href="./?update-profile<?php $idForAdmin->display(); ?>"><button>Update Profile</button></a>
 
-<?php if($id === $_SESSION["userID"]): ?>
-  <a href="./?update-password"><button>Update Password</button></a>
-  <a href="./processing.php?sign-out"><button>Log Out</button></a>
-<?php endif; ?>
-
-<?php if(checkSession("is_admin")): ?>
-  <a href="./processing.php?delete-account&id=<?= $user["id"] ?>">
-    <button class="btn-red">Delete Account</button>
-  </a>
-<?php endif; ?>
+<?php
+$userOnlyLinks->display();
+$deleteAccountButton->display();
+?>
