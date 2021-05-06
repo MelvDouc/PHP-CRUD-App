30e0f87a7a0e577ceaf52d687b3c9c5555f83f6d
unknown <melv.douc@gmail.com>
2021-05-06 03:52:24 +0200

    db auto created

--- db.php
+++ db.php
@@ -1,10 +1,15 @@
 <?php
 
+try {
   $host = "localhost";
   $db_name = "crud";
   $user = "root";
   $password = "";
 
   $db = new PDO("mysql:host=$host;dbname=$db_name;charset=utf8", $user, $password);
-
-?>
+  $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+  $query = file_get_contents("users.sql");
+  $db->exec($query);
+} catch (PDOException $e) {
+  echo $e->getMessage();
+}
